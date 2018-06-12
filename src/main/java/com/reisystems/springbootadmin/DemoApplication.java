7204fc5354d07f4c90c766938cefe5ba4cee3a29
ykhadilkar-rei <ykhadilkar@reisys.com>
2018-06-12 15:31:15 -0400

    first commit

---
+++ src/main/java/com/reisystems/springbootadmin/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.reisystems.springbootadmin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAdminServer
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
